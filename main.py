39d6cc49cc2ddd5add496f74d1b728de7a84851b
teacher-NT <holmonbek@gmail.com>
2025-11-10 09:15:40 +0500

    New

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("Bu github uchun kod")
+import os
+os.system("cls")
 
-fjvnsd
+car1 = {
+    "brand": "GM",
+    "model": "Maluba",
+    "rang": "Qora",
+    "yil": 2025,
+    "probeg": 10_000,
+    3:4,
+    
+}
+car1['narxi'] = 20_000
+
+print(car1)
+car1["model"] = 'Malibu'
+print(car1["model"])
