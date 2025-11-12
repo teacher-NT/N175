02ec1b741668396ad2776036769375483e103a47
teacher-NT <holmonbek@gmail.com>
2025-11-12 09:14:16 +0500

    5-masala

--- main.py
+++ main.py
@@ -1,22 +1,74 @@
 import os
 os.system("cls")
-
-speed = {
-    "Tesla": 250,
-    "BMW": 240,
-    "Mercedes": 260,
-    "Audi": 230
+books = {
+    "O'tkan kunlar": {
+        "muallifi": "Abdulla Qodiriy",
+        "janri": "Roman",
+        "chop etilgan yili": 1926,
+        "tarjimalar soni": 5
+    },
+    "Mehrobdan chayon": {
+        "muallifi": "Abdulla Qodiriy",
+        "janri": "Roman",
+        "chop etilgan yili": 1929,
+        "tarjimalar soni": 3
+    },
+    "Kecha va kunduz": {
+        "muallifi": "Cho'lpon",
+        "janri": "Roman",
+        "chop etilgan yili": 1934,
+        "tarjimalar soni": 4
+    },
+    "Sarob": {
+        "muallifi": "Abdulla Qahhor",
+        "janri": "Roman",
+        "chop etilgan yili": 1935,
+        "tarjimalar soni": 2
+    },
+    "Ulug'bek xazinasi": {
+        "muallifi": "Odil Yoqubov",
+        "janri": "Tarixiy roman",
+        "chop etilgan yili": 1974,
+        "tarjimalar soni": 6
+    },
+    "Don Kixot": {
+        "muallifi": "Migel de Servantes",
+        "janri": "Roman",
+        "chop etilgan yili": 1605,
+        "tarjimalar soni": 50
+    },
+    "Urush va tinchlik": {
+        "muallifi": "Lev Tolstoy",
+        "janri": "Tarixiy roman",
+        "chop etilgan yili": 1869,
+        "tarjimalar soni": 45
+    },
+    "Alkimyogar": {
+        "muallifi": "Paulo Koelyo",
+        "janri": "Falsafiy roman",
+        "chop etilgan yili": 1988,
+        "tarjimalar soni": 80
+    },
+    "1984": {
+        "muallifi": "Jorj Oruell",
+        "janri": "Antiutopik roman",
+        "chop etilgan yili": 1949,
+        "tarjimalar soni": 65
+    },
+    "Kichkina shahzoda": {
+        "muallifi": "Antuan de Sent-Ekzyuperi",
+        "janri": "Falsafiy ertak",
+        "chop etilgan yili": 1943,
+        "tarjimalar soni": 300
+    }
 }
 
-lst = []
-for k,v in speed.items():
-    
-        for i in range(len(lst)):
-            if lst[i][1] > v:
-                lst.insert(i, [k,v])
-                break
-        else:
-            lst.append([k,v])
-            
-print(lst[::-1])
+name = 'urush Va Tinchlik'
 
+# print(books[name])
+for book in books:
+    if name.lower() == book.lower():
+        print(books[book])
+        break
+else:
+    print("Kitob topilmadi")
