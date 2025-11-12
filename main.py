1da4fc4094ab4848ea8533872677191615f30116
teacher-NT <holmonbek@gmail.com>
2025-11-12 09:03:09 +0500

    3-masala

--- main.py
+++ main.py
@@ -1,44 +1,22 @@
 import os
 os.system("cls")
 
-car1 = {
-    "brand": "GM",
-    "model": "Malibu",
-    "rang": "Qora",
-    "yil": 2025,
-    "probeg": 10_000,
-    3:4,
-    
+speed = {
+    "Tesla": 250,
+    "BMW": 240,
+    "Mercedes": 260,
+    "Audi": 230
 }
 
-# print(car1['Brand'])
-# print(car1.get("Brand", 'Bunday brand yoq'))
+lst = []
+for k,v in speed.items():
+    
+        for i in range(len(lst)):
+            if lst[i][1] > v:
+                lst.insert(i, [k,v])
+                break
+        else:
+            lst.append([k,v])
+            
+print(lst[::-1])
 
-# print(car1.keys())
-
-# print(car1.values())
-
-# print(car1.pop('yil'))
-# print(car1)
-
-# print(car1.items())
-# for k, v in car1.items():
-#     print(k, v)
-
-# car2 = car1
-# print(car2)
-# car2 = car1.copy()
-
-# car1.popitem()
-# car1.popitem()
-# print(car1)
-
-
-car1['Narx'] = 20_000
-car1["model"] = 'Cobalt'
-car1['rang'] = 'oq'
-
-new_data = {'Narx': 20_000, 'model':'Cobalt', 'rang':'oq'}
-car1.update(new_data)
-
-print(car1)
