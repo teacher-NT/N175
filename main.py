8cc3bc9430ab33a206bf9ff61bc9d9f84470c73f
teacher-NT <holmonbek@gmail.com>
2025-11-24 09:10:17 +0500

    all

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import os
 os.system("cls")
 
-# sonlar = [12,3,4,15,3,5,44,2,4,8,67]
+import requests as rq
 
-# def func(n):
-#     return n<5
+url = 'https://cbu.uz/uz/arkhiv-kursov-valyut/json/'
 
-# result = list(filter(func, sonlar))
+data = rq.get(url).json()
 
-# print(result)
-
-
-yoshlar = [12,19,18,20,14]
-def func(n):
-    return n+1
-
-result = list(map(func, yoshlar))
-print(result)
+print(data[0])
