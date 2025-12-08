0ea222929cbddb37937bcaf93cc32474dd658ede
Mirjamol Mirislomov <alpholmon@gmail.com>
2025-12-08 09:43:10 +0500

    PyQt

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import os
 os.system("cls")
 
-class A:
-    def func(self):
-        print("A")
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.QtGui import QFont
+font1 = QFont("Arial", 30)
 
-class B(A):
-    def func(self):
-        print("B")
+app = QApplication([])
 
-class C(B, A):
-    pass
+window = QWidget()
+window.setWindowTitle("Mening birinchi oynali dasturim")
+window.setGeometry(400,400, 920, 600)
+window.setStyleSheet("background-color: #4bfc1e;")
 
-class D(C, A):
-    pass
+label = QLabel(window)
+label.setText("Samandarning qo'li sinibdi")
+label.setFont(font1)
+label.setFixedWidth(920)
+label.move(50, 50)
+label.setStyleSheet("color: white;")
 
-class E(D, A):
-    pass
+def func1():
+    global label
+    label.setText("Samandar darsda uhlab qoldi")
 
-e1 =  E()
-e1.func()
+btn1 = QPushButton(window)
+btn1.setText("Press me!")
+btn1.setGeometry(400,150, 100, 50)
+btn1.setStyleSheet("background-color: white; font-size:20px;")
+btn1.clicked.connect(func1)
+
+window.show()
+
+app.exec_()
