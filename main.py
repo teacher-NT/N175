3d9a2e52bc245f03e838bc099a81d4a938704b1b
Mirjamol Mirislomov <alpholmon@gmail.com>
2025-12-22 09:52:54 +0500

    Insert into

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
 import os
 os.system("cls")
 
-from PyQt5.QtWidgets import (
-    QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+# from PyQt5.QtWidgets import (
+#     QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+# )
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='1234',
+    database='dars_data'
 )
 
-class SecondWindow(QWidget):
-    def __init__(self, num):
-        super().__init__()
-        self.setFixedSize(900,600)
-        label1 = QLabel(self)
-        label1.setText(f"Men {num}-oynaman")
-        label1.setStyleSheet("font-size:30px;")
+cursor = mydb.cursor()
+# query = """SELECT * FROM people 
+#             WHERE FirstName LIKE 'A%';
+#         """
+first = input("Ism:")
+last = input("Familya:")
+gen = input("Jins:")
+email = input("Email:")
+phone = input("Phone:")
+birth = input("Birth day: ")
+job = input("Job: ")
+add = input("Address: ")
+query = f"""INSERT INTO people ( FirstName, LastName, Gender, Email, Phone, BirthDay, JobTitle, Address)
+            VALUES
+            ('{first}', '{last}', '{gen}', '{email}', '{phone}', '{birth}', '{job}', '{add}');
+        """
+cursor.execute(query)
+mydb.commit()
+
+print(cursor.rowcount, "ta qator o'zgardi!")
 
 
 
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.windows = []
-        self.setFixedSize(600,900)
-        layout = QVBoxLayout()
-        label1 = QLabel("Bu asosiy oyna")
-        label1.setStyleSheet("font-size:20px;")
-        layout.addWidget(label1)
-        btn1 = QPushButton("Keyingi oynani ochish")
-        btn1.setStyleSheet("background-color: green; color:white;font-size:12px;")
-        btn1.clicked.connect(self.open_other_window)
-        layout.addWidget(btn1)
-        self.setLayout(layout)
-        self.show()
-    
-    def open_other_window(self):
-        num = len(self.windows)+1
-        win = MainWindow()
-        self.windows.append(win)
-        self.windows[-1].show()
 
 
-app = QApplication([])
-win = MainWindow()
-app.exec_()
+
+
+
+
+
+
+
+
+
+
+
+# app = QApplication([])
+# app.exec_()
